3524bd81e4fda8e95164792a58f0ff6757ae10b0
Austin Quach <austinquachh@gmail.com>
2015-05-03 22:05:41 -0700

    Revert "Settings: forward port HeadsUp options (2/2)"
    This reverts commit 3e2e047d1ebb03b5b6a5e737d490c12778f48a35.

--- src/com/android/settings/notification/NotificationSettings.java
+++ src/com/android/settings/notification/NotificationSettings.java
@@ -106,7 +106,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
     private boolean mSecure;
     private int mLockscreenSelectedValue;
     private Preference mAlarmRingtonePreference;
-    private Preference mHeadsUp;
     private ComponentName mSuppressor;
     private int mRingerMode = -1;
     private CheckBoxPreference mVolumeLinkNotification;
@@ -167,8 +166,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
         refreshNotificationListeners();
-
-        mHeadsUp = findPreference(Settings.System.HEADS_UP_NOTIFICATION);
         updateRingerMode();
         updateEffectsSuppressor();
     }
@@ -187,11 +184,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         for (VolumeSeekBarPreference volumePref : mVolumePrefs) {
             volumePref.onActivityResume();
         }
-
-        boolean headsUpEnabled = Settings.System.getInt(
-                getContentResolver(), Settings.System.HEADS_UP_NOTIFICATION, 0) == 1;
-        mHeadsUp.setSummary(headsUpEnabled
-                ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 
     @Override
